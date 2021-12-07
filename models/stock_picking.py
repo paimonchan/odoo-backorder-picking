02f530137b1e0398daafed80aa334b1faaedb65c
Paimon <n.h.shounen@gmail.com>
2021-12-07 22:18:49 +0700

    return picking itself instead new picking

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -5,3 +5,9 @@ from odoo import models
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
+
+    def _create_backorder(self):
+        picking_ids = self.env['stock.picking']
+        for record in self:
+            picking_ids |= record
+        return picking_ids
