74095c58331bacefd2b018780ab5345c758bcf40
Paimon <n.h.shounen@gmail.com>
2021-12-08 21:38:20 +0700

    add option to disable merge backorder

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -7,6 +7,10 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     def _create_backorder(self):
+        param = self.env['ir.config_parameter'].sudo()
+        disable_merge_backorder = param.get_param('paimon.disable_merge_backorder')
+        if disable_merge_backorder:
+            return super()._create_backorder()
         picking_ids = self.env['stock.picking']
         for record in self:
             picking_ids |= record
